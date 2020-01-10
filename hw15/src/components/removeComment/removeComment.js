ac7390999de0df1974d387e0914945a6fbc5e4da
Stephan <stephan.startsev@gmail.com>
2020-01-10 16:57:13 +0300

    remedy

--- hw15/src/components/removeComment/removeComment.js
+++ hw15/src/components/removeComment/removeComment.js
@@ -1,19 +1,21 @@
 import React from 'react'
 
-export default class RemoveComment extends React.Component {
-  render() {
-    const{comments, onRemoveComment} = this.props
-    const markup = comments.map(comment => 
+const RemoveComment = (props) => {
+
+  const{comments, onRemoveComment} = props
+
+  const markup = comments.map(comment => 
     <div key={comment.id}>
       <span>{comment.id} - {new Date().toLocaleTimeString()}: </span>
       <strong>{comment.title}, </strong>
       <span>{comment.comment}</span>
       <button onClick={onRemoveComment.bind(null, comment.id)}>Удалить комментарий</button>
     </div>)
-    return (
-      <div>
-        {markup}
-      </div>
-    )
-  }
+  return (
+    <div>
+      {markup}
+    </div>
+  )
 }
+
+export default RemoveComment

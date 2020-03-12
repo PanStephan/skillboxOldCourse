a095e226e6a6e5209e6ed350af7ae71938ecde77
Stephan <stephan.startsev@gmail.com>
2020-03-12 13:39:13 +0300

    todo-app

--- hw8/hw8.js
+++
@@ -1,5 +0,0 @@
-function filterByType(type, ...arr){
-    return arr.filter(e => typeof e === type)
-}
-let obj = filterByType ('string', 10, 20, 'a', 'b', true, false);
-alert(JSON.stringify(obj));
